7e16ba1a3e3d657f6371a69aaa1ccdc22cd5a86e
ikpixels <ikpixels.py@gmail.com>
2026-03-22 10:21:53 +0200

    lero settings

--- akuno_project/settings.py
+++ akuno_project/settings.py
@@ -15,7 +15,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 DEBUG = os.environ.get("DEBUG") == "True"
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
