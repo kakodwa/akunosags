eded58f67f0eab9091fcdcb26b2f7dedaaac78c8
ikpixels <ikpixels.py@gmail.com>
2026-03-31 21:08:04 +0200

    admin

--- akuno_project/urls.py
+++ akuno_project/urls.py
@@ -5,6 +5,12 @@ from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 
+
+admin.site.index_title= 'Akunosags Site Administration - For Support call/Wap 0993344416(Isaac Kakodwa)'
+admin.site.site_header = 'Akunosags Administration'
+admin.site.site_title = "Akunosags Admin Portal"
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
